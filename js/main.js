c9c2bf7f6f2eb3e19bcdd6f38958e2c603648d6e
Abdelrahman Nasser <abdelrahman.nasseryo@gmail.com>
2023-01-01 15:34:05 +0200

    Finish Team Section

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ let subMenu = document.querySelector(".sub-menu");
 let megaMenu = document.querySelector(".mega-menu");
 let menuBar = document.querySelector(".menu-bar");
 let navLinks = document.querySelectorAll("nav a:not(.sub-menu)");
+let noneLinks = document.querySelectorAll("a[href='#']:not(.sub-menu)");
+console.log();
 function toggleActive() {
   subMenu.classList.toggle("active");
   megaMenu.classList.toggle("active");
@@ -19,5 +21,12 @@ navLinks.forEach((link) => {
     if (megaMenu.classList.contains("active")) {
       toggleActive();
     }
+  };
+});
+
+// Prevent Links Default Behvior
+noneLinks.forEach((link) => {
+  link.onclick = (e) => {
+    e.preventDefault();
   };
 });
