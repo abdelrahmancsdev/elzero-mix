9c5eaba13c2a753f4cd82cfebf9f50bf746ba03a
Abdelrahman Nasser <abdelrahman.nasseryo@gmail.com>
2023-07-06 16:52:28 +0300

    Create Popup Functionality

--- js/main.js
+++ js/main.js
@@ -99,3 +99,71 @@ setInterval(() => {
   document.querySelector(".seconds").innerHTML =
     seconds < 10 ? `0${seconds}` : seconds;
 }, 1000);
+
+// Popup
+function openPopup(popElement, type) {
+  let popup = document.createElement("div");
+  popup.classList.add("popup-box");
+  let popupOverlay = document.createElement("div");
+  popupOverlay.classList.add("popup-overlay");
+  let closePopup = document.createElement("span");
+  closePopup.classList.add("close-popup");
+  let popupContent = document.createElement("div");
+  popupContent.classList.add("popup-content");
+  closePopup.textContent = "X";
+  closePopup.onclick = () => {
+    popup.remove();
+    popupOverlay.remove();
+    document.body.classList.remove("popup-active");
+  };
+  popupContent.appendChild(popElement);
+  if (type != "gallery") {
+    let loremPara = document.createElement("p");
+    loremPara.textContent =
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quis architecto ipsa debitis magni sequi consequuntur deleniti dolorum in voluptatem, deserunt eum reiciendis velit minima id amet facere! Magnam, consequatur.";
+    popupContent.appendChild(loremPara);
+  }
+  popup.appendChild(closePopup);
+  popup.appendChild(popupContent);
+  document.body.appendChild(popup);
+  document.body.appendChild(popupOverlay);
+  document.body.classList.add("popup-active");
+}
+
+// Gallery Popup
+let galleryImages = document.querySelectorAll(".image-box .image");
+galleryImages.forEach((img) => {
+  img.onclick = () => {
+    let currentImage = img.firstElementChild.cloneNode(true);
+    openPopup(currentImage, "gallery");
+  };
+});
+
+// Features Popup
+let featBtn = document.querySelectorAll("#features .feat .more");
+featBtn.forEach((btn) => {
+  btn.onclick = () => {
+    let currentFeat = btn.parentElement.parentElement.cloneNode(true);
+    openPopup(currentFeat, "feature");
+  };
+});
+
+// Services Popup
+let servBtn = document.querySelectorAll("#services .serv .details");
+servBtn.forEach((btn) => {
+  btn.onclick = (e) => {
+    e.preventDefault();
+    let currentServ = btn.parentElement.parentElement.cloneNode(true);
+    openPopup(currentServ, "service");
+  };
+});
+
+// Articles Popup
+let postBtn = document.querySelectorAll("#articles .post .more");
+postBtn.forEach((btn) => {
+  btn.onclick = (e) => {
+    e.preventDefault();
+    let currentPost = btn.parentElement.parentElement.cloneNode(true);
+    openPopup(currentPost, "article");
+  };
+});
